fad7084cae9d8d7e3c86901c7366bb7cf00d08c4
Alexander Dadukin <hi@st235.xyz>
2020-04-18 21:54:32 +0300

    implemented cycle detection.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
+#include <unordered_set>
 
 #include "./src/RandomFactory.h"
+
+int detectCycle(random_generator::Random* random);
 
 int main() {
     auto random_factory = std::make_unique<random_generator::RandomFactory>();
     auto random_ptr = random_factory->create();
 
-    std::cout << random_ptr->nextInt();
+    std::cout << "Cycle size is: " << detectCycle(random_ptr.get());
 
     return 0;
+}
+
+int detectCycle(random_generator::Random* random) {
+    std::unordered_set<uint32_t> lookup;
+    uint32_t current_value = random->nextInt();
+    while (lookup.find(current_value) == lookup.end()) {
+        lookup.insert(current_value);
+        current_value = random->nextInt();
+    }
+    return lookup.size();
 }
